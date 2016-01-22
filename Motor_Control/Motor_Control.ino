ab3b5294bc1dc5f03ab14ab2a923ebf4e32e3cc0
Wolfdog18 <badgerwolfpack@gmail.com>
2016-01-22 13:42:25 -0600

    Added lcd

--- Motor_Control/Motor_Control.ino
+++ Motor_Control/Motor_Control.ino
@@ -4,6 +4,23 @@ int potL, potR;
 
 Servo Right;
 Servo Left;
+
+#include <Wire.h>
+#include <LCD.h>
+#include <LiquidCrystal_I2C.h>
+
+#define I2C_ADDR    0x27  // Define I2C Address where the PCF8574A is
+#define BACKLIGHT_PIN     3
+#define En_pin  2
+#define Rw_pin  1
+#define Rs_pin  0
+#define D4_pin  4
+#define D5_pin  5
+#define D6_pin  6
+#define D7_pin  7
+
+LiquidCrystal_I2C  lcd(I2C_ADDR,En_pin,Rw_pin,Rs_pin,D4_pin,D5_pin,D6_pin,D7_pin);
+
   
 void setup() {
   pinMode(A0, INPUT);
@@ -14,6 +31,16 @@ void setup() {
   Right.attach(11);
   Left.attach(10);
   Serial.begin(9600);
+
+  lcd.begin (20,4);
+
+// Switch on the backlight
+  lcd.setBacklightPin(BACKLIGHT_PIN,POSITIVE);
+  lcd.on();
+  lcd.setCursor(0, 0);
+  lcd.print("Left: ");
+  lcd.setCursor(0, 1);
+  lcd.print("Right: ");
 }
 
 void loop() {
